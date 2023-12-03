d27558ec45826db24b39f1c0f518fcf0e2e2da99
Bald Eagle <beagle@gmail.com>
2023-12-03 15:09:58 +0400

    add detailed information and base is almost ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { Octokit } from "octokit";
+import sun from "./assets/icon-sun.svg";
+import moon from "./assets/icon-moon.svg";
+import search from "./assets/icon-search.svg";
+import location from "./assets/icon-location.svg";
+import website from "./assets/icon-website.svg";
+import twitter from "./assets/icon-twitter.svg";
+import company from "./assets/icon-company.svg";
 
 function App() {
   const TOKEN =
@@ -9,7 +16,20 @@ function App() {
     auth: TOKEN,
   });
 
-  const [dataList, setDataList] = useState([]);
+  const [dataList, setDataList] = useState({
+    avatar_url: "",
+    name: "",
+    login: "",
+    created_at: "",
+    bio: "",
+    public_repos: 0,
+    followers: 0,
+    following: 0,
+    location: "",
+    blog: "",
+    twitter_username: "",
+    company: "",
+  });
 
   useEffect(() => {
     octokit
@@ -26,42 +46,74 @@ function App() {
         return res.data;
       })
       .then((data) => {
-        let avatar_url = data.avatar_url;
-        let name = data.name;
-        let login = data.login;
-        let created_at = data.created_at;
-        let bio = data.bio;
-        let public_repos = data.public_repos;
-        let followers = data.followers;
-        let following = data.following;
-        let location = data.location;
-        let blog = data.blog;
-        let twitter_username = data.twitter_username;
-        let company = data.company;
-        let dataList = [
-          avatar_url,
-          name,
-          login,
-          created_at,
-          bio,
-          public_repos,
-          followers,
-          following,
-          location,
-          blog,
-          twitter_username,
-          company,
-        ];
-        setDataList(dataList);
+        setDataList({
+          avatar_url: data.avatar_url,
+          name: data.name,
+          login: data.login,
+          created_at: data.created_at,
+          bio: data.bio,
+          public_repos: data.public_repos,
+          followers: data.followers,
+          following: data.following,
+          location: data.location,
+          blog: data.blog,
+          twitter_username: data.twitter_username,
+          company: data.company,
+        });
       });
   }, []);
 
-  console.log("render");
   return (
     <div className="App">
-      {dataList.map((i) => (
-        <h1 key={crypto.randomUUID()}>{i}</h1>
-      ))}
+      <div className="header">
+        <h1>devfinder</h1>
+        <div>
+          <h4>DARK</h4>
+          <img src={moon} alt="" />
+        </div>
+        <div>
+          <img src={search} alt="" />
+          <input type="text" placeholder="Search GitHub username…" />
+          <button>search</button>
+        </div>
+      </div>
+      <div className="main-div">
+        <img src={dataList.avatar_url} alt="" />
+        <h1>{dataList.name}</h1>
+        <p>{dataList.login}</p>
+        <p>{dataList.created_at}</p>
+        <p>{dataList.bio}</p>
+        <div className="stats">
+          <div>
+            <p>repos</p>
+            <h6>{dataList.public_repos}</h6>
+          </div>
+          <div>
+            <p>followers</p>
+            <h6>{dataList.followers}</h6>
+          </div>
+          <div>
+            <p>following</p>
+            <h6>{dataList.following}</h6>
+          </div>
+        </div>
+        <div>
+          <img src={location} />
+          <h4>{dataList.location}</h4>
+        </div>
+        <div>
+          <img src={website} />
+          <h4>{dataList.website}</h4>
+        </div>
+        <div>
+          <img src={twitter} />
+          <h4>{dataList.twitter}</h4>
+        </div>
+        <div>
+          <img src={company} />
+          <h4>{dataList.company}</h4>
+        </div>
+      </div>
     </div>
   );
 }
